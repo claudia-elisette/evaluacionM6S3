920296babf9baf316272514f73a1ec2cf6d08243
Claudia Elisette <claudiae.urbinag@gmail.com>
2024-09-06 11:26:59 -0400

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from '../../node_modules/bootstrap-vue'
-import '../../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from '../node_modules/bootstrap-vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
